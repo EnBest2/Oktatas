bc2d700d72f425214fdbbf021426e7278153303d
EnBest2 <enbest2@gmail.com>
2025-03-27 12:41:05 +0100

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-
 // Supabase inicializálás
+const { createClient } = supabase;
+
 const SUPABASE_URL = 'https://ejrucgtmgiwpfexbvwxa.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqcnVjZ3RtZ2l3cGZleGJ2d3hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwNzEzMTYsImV4cCI6MjA1ODY0NzMxNn0.1J_eWcCSeJLdSNDDLNksr6TaQc3wCHKBRVc3a5pTnJQ';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 let selectedCategory = '';
 
@@ -22,7 +23,7 @@ async function handleUpload() {
   }
 
   const { data, error } = await supabase.storage
-    .from('adatok')  // <-- frissített bucket név
+    .from('adatok')
     .upload(`${selectedCategory}/${file.name}`, file, {
       cacheControl: '3600',
       upsert: true
